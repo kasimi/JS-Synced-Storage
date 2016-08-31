b820d916b931ca60d78d40c72544d69057590a8f
kasimi <mail@kasimi.net>
2016-08-31 15:26:30 +0200

    Initial commit v1.0.0

---
+++ README.md
@@ -0,0 +1,50 @@
+# Synced Storage
+
+Utility function for synchronizing data between browser windows/tabs using Web Storage and vanilla JS.
+
+## What is this?
+
+Frequently checking for new data by sending requests to a server is a common thing to do. When multiple windows/tabs are open, requests are sent in an isolated way: window A just fetched and processed new data, window B is still waiting for its next update to be triggered. This presents us with two problems and opportunities for optimization:
+1. The server receives more requests from the client than necessary. There should be a way to limit the number of requests so that it is enforced that no more than one request is sent in a given `updateInterval`.
+2. When a window/tab receives new data, it should send that data to other windows/tabs in order for them to process it immediately and not having to wait for their update to be triggered.
+
+The `syncedStorage()` function solves these issues.
+
+## Documentation
+
+Available options to pass to the `syncedStorage()` function:
+
+| Option | Required | Type | Description |
+|---|---|---|---|
+| `getData` | Yes | `function` | Callback function that is called when new data needs to be fetched. It receives a callback function as its first argument that should be called with the cacheable data as first argument. Do not return the data from within getData! |
+| `processData` | Yes | `function` | Callback function that processes new data. It receives the data as its first argument. |
+| `updateInterval` | Yes | `int` | Number of milliseconds after which the data should be updated. |
+| `sessionLength` | No, default: `false` | `int|bool` | Number of milliseconds after which to stop updating the data. If `false` is passed, updates will never stop. |
+| `storage` | No, default: `localStorage` | `object` | The storage object to use. |
+| `storageKey` | No, default: `'syncedStorage'` | `string` | String with which to associate the data in the storage. |
+
+## Example
+
+```javascript
+syncedStorage({
+	getData: function(accept) {
+		jQuery.getJSON('/api/json/update').done(accept);
+	},
+	processData: function(data) {
+		jQuery('<div/>', {text: data.text}).appendTo(document);
+	},
+	updateInterval: 10 * 1000, // Update every 10 seconds
+	sessionLength: 60 * 1000, // Stop updating after 1 minute
+	storageKey: 'syncedStorageExample'
+});
+
+```
+
+## Changelog
+
+* v1.0.0
+  * initial release
+
+## Copyright and license
+
+Copyright 2016 kasimi. Released under the [MIT license](https://github.com/kasimi/JS-Synced-Storage/blob/master/LICENSE).
---
+++ synced-storage.js
@@ -0,0 +1,92 @@
+/*!
+ *
+ * Synced Storage v1.0.0
+ *
+ * https://github.com/kasimi/JS-Synced-Storage
+ * Copyright 2016 kasimi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+var syncedStorage = function(options) {
+
+	"use strict";
+
+	var mergeFlatDefensive = function(obj, mergingObj) {
+		for (var key in mergingObj) {
+			if (mergingObj.hasOwnProperty(key) && !obj.hasOwnProperty(key)) {
+				obj[key] = mergingObj[key];
+			}
+		}
+	};
+
+	mergeFlatDefensive(options, {
+		storage			: localStorage,
+		storageKey		: 'syncedStorage',
+		sessionLength	: false,
+		cachedDataTTL	: options.updateInterval
+	});
+
+	var sessionLength = options.sessionLength;
+	var lastStorageEventTime = 0;
+
+	var writeToStorage = function(data) {
+		var storageContent = JSON.stringify({
+			time: new Date().getTime(),
+			data: data
+		});
+		options.storage.setItem(options.storageKey, storageContent);
+	};
+
+	var readFromStorage = function() {
+		var storageContent = options.storage.getItem(options.storageKey);
+		return JSON.parse(storageContent).data;
+	};
+
+	var updateData = function(done) {
+		if (lastStorageEventTime > new Date().getTime() - options.updateInterval) {
+			done();
+		} else {
+			options.getData(function(data) {
+				writeToStorage(data);
+				options.processData(data);
+				done();
+			});
+		}
+	};
+
+	var scheduleUpdate = function() {
+		setTimeout(function() {
+			updateData(function() {
+				if (sessionLength === false || (sessionLength -= options.updateInterval) > 0) {
+					scheduleUpdate();
+				}
+			});
+		}, options.updateInterval);
+	};
+
+	window.addEventListener('storage', function(e) {
+		if (e.key == options.storageKey) {
+			lastStorageEventTime = new Date().getTime();
+			options.processData(readFromStorage());
+		}
+	});
+
+	scheduleUpdate();
+};
